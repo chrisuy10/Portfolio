2063167774e63f6ddecd4d3001fa0359a229425c
benedict uy <87054833+chrisuy10@users.noreply.github.com>
2023-04-28 13:40:56 +0800

    update pages, nav, header

---
+++ mailsender/header.php
@@ -0,0 +1,73 @@
+<?php
+include  "function.php";
+
+//for testing http://mailsender.test/reference-code/mailsender/
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>RFID Attendance</title>
+    <meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <style>
+		.navbar {
+			background-color: #4CAF50;
+		}
+
+		.nav-link {
+			color: #FFFFFF;
+		}
+
+		.footer {
+			background-color: #4CAF50;
+			color: #FFFFFF;
+			padding: 20px;
+		}
+
+		.card {
+			background-color: #fff;
+			border-radius: 5px;
+			box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2);
+			margin: 20px auto;
+			max-width: 400px;
+			padding: 20px;
+			text-align: center;
+		}
+
+		.card h1 {
+			font-size: 1.5em;
+			margin-bottom: 0.5em;
+		}
+
+		.card p {
+			font-size: 1.2em;
+		}
+
+		form {
+			margin-top: 20px;
+		}
+        body {
+			background-color: #f2f2f2;
+		}
+		.container {
+			background-color: white;
+			margin-top: 50px;
+			padding: 50px;
+			border-radius: 10px;
+			box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+		}
+		h1 {
+			text-align: center;
+			margin-bottom: 30px;
+		}
+		input[type="submit"] {
+			margin-top: 20px;
+		}
+	</style>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+   
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+	
+</head>
---
+++ mailsender/page-register.php
@@ -0,0 +1,85 @@
+<?php include "header.php" ?>
+<body>
+	<?php include "nav.php"; ?>
+    
+    <div id="enroll" class="container">
+		<h1>Register Student Details</h1><hr>
+		<form action="" method="post" enctype="multipart/form-data">
+			<div class="form-group">
+				<label for="yearsem">Year and Semester:</label>
+				<select class="form-control col-md-4" id="yearsem" name="yearsem">
+					<option value="">Select Year and Semester</option>
+					<option value="2023-1ST">2023-1ST</option>
+					<option value="2023-2ND">2023-2ND</option>
+					<option value="2024-1ST">2024-1ST</option>
+					<option value="2024-2ND">2024-2ND</option>
+
+				</select>
+			</div>
+
+
+			<div class="form-group row">
+                <div class="col-md-6">
+                    <label for="firstname">First Name:</label>
+                    <input type="text" class="form-control" id="firstname" name="firstname">
+                </div>
+                <div class="col-md-6">
+                    <label for="lastname">Last Name:</label>
+                    <input type="text" class="form-control" id="lastname" name="lastname">
+                </div>
+            </div>
+
+			<div class="form-group row">
+                <div class="col-md-12">
+                    <label for="address">Complete Address:</label>
+                    <input type="text" class="form-control" id="address" name="address">
+                </div>
+            </div>
+
+            <div class="form-group row">
+                <div class="col-md-6">
+                    <label for="studentID">Student ID:</label>
+                    <input type="text" class="form-control" id="studentID" name="studentID">
+                </div>
+                <div class="col-md-6">
+                    <label for="course">Course:</label>
+                    <input type="text" class="form-control" id="course" name="course">
+                </div>
+            </div>
+			<div class="form-group row">
+                <div class="col-md-6">
+                    <label for="phone">Emergency Contact No.:</label>
+                    <input type="text" class="form-control" id="phone" name="phone">
+                </div>
+                <div class="col-md-6">
+                    <label for="rfid">RFID:</label>
+                    <input type="text" class="form-control" id="rfid" name="rfid">
+                </div>
+            </div>
+			<div class="form-group row">
+				<div class="col-md-6">
+					<input class="form-control" type="file" name="profile-pic" id="profile-pic">
+                </div>				
+            </div>
+
+			<input type="submit" class="btn btn-primary" name="enroll" value="Submit">
+		</form>
+        <?php 
+            if (isset($_POST['enroll'])) {
+                insert($conn);
+				echo $insert_result;
+				echo "<script>document.getElementById('enroll').scrollIntoView();</script>";
+            }
+        ?>
+	</div>
+
+</body>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script>
+    var divContent = document.getElementById("response").innerHTML;
+
+    // Set the content to the hidden input field
+    document.getElementById("content").value = divContent;
+</script>
