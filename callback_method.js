5634e98b839f27728246231d067de71bd1ab8e14
benedict uy <87054833+chrisuy10@users.noreply.github.com>
2023-05-30 10:00:02 +0800

    add new reference code

---
+++ callback_method.js
@@ -0,0 +1,24 @@
+const waitForElementLoad = (callback) => {
+    const checkElementsLoaded = () => {
+      const nextLoaded = document.querySelector('.slick-next') !== null;
+
+      
+      if (nextLoaded) {
+        callback(); // Execute the callback if elements are loaded
+        console.log('loaded')
+      } else {
+        setTimeout(checkElementsLoaded, 100); // Check again after 100 milliseconds
+        console.log('loading')
+      }
+    };
+  
+    checkElementsLoaded();
+  };
+  waitForElementLoad(() => {
+    //do something
+    // Set an interval to trigger the next slide every 3 seconds
+    const nextbutton = document.querySelector('.slick-next');
+    const intervalId = setInterval(function() {
+      nextbutton.click();
+    }, 3000);
+  });
